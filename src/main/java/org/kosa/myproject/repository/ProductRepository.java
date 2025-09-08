c183201ead67ac2f24ea58d17998843511a19a41
seo-inst <innerpeace.deepi@gmail.com>
2025-09-08 11:51:09 +0900

    feat:product entity2

--- src/main/java/org/kosa/myproject/repository/ProductRepository.java
+++ src/main/java/org/kosa/myproject/repository/ProductRepository.java
@@ -4,10 +4,18 @@ import org.kosa.myproject.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 // <Product,Long> : Product 는 관리 엔티티, Long 은 Primary key에 대응하는 필드의 타입
 @Repository // Spring 데이터 접근 Data Access Layer(영속성 계층) 컴포넌트 임을 명시
 public interface ProductRepository  extends JpaRepository<Product,Long> {
     // 기본 CRUD 는 JpaRepository 가 제공 : save() , saveAll() , count() , findAll(), findById() , delete() 등
+    /*
+            상품명 name 으로 조회
+            메서드 명명 규칙 : find + By + 엔티티필드명
+            SQL : SELECT * FROM products WHERE product_name = ?
+     */
+    Optional<Product> findByName(String 노트북);
 }
 
 
--- src/test/java/org/kosa/myproject/repository/test/ProductRepositoryTest.java
+++ src/test/java/org/kosa/myproject/repository/test/ProductRepositoryTest.java
@@ -1,4 +1,38 @@
 package org.kosa.myproject.repository.test;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.kosa.myproject.entity.Product;
+import org.kosa.myproject.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional // 테스트에서 이 애너테이션을 명시하면 테스트 후 자동 롤백
+@Slf4j
 public class ProductRepositoryTest {
+        @Autowired
+        ProductRepository productRepository;
+        @Test
+        void testFindByName(){
+            //Given
+            productRepository.save(Product.builder()
+                    .name("노트북")
+                    .price(150000)
+                    .stockQuantity(50)
+                    .build()
+            );
+            //When  상품명으로 조회
+            Optional<Product> product = productRepository.findByName("노트북");
+            //Then
+            assertThat(product).isPresent();
+        }
 }
+
+
+
